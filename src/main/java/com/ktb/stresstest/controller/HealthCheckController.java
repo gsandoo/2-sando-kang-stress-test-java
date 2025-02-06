873ee6d1b3a22ba490c18b3b8178ad545b3737cc
sakang <rogersddt@naver.com>
2025-02-06 17:33:36 +0900

    Feat: Health Check

--- src/main/java/com/ktb/stresstest/controller/HealthCheckController.java
+++ src/main/java/com/ktb/stresstest/controller/HealthCheckController.java
@@ -1,7 +1,9 @@
 package com.ktb.stresstest.controller;
 
+import com.ktb.stresstest.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +12,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/health")
 public class HealthCheckController {
+
+    @GetMapping("")
+    public ResponseDto<?> healthCheck(){
+        return ResponseDto.ok("health 체크 성공");
+    }
+
+    @GetMapping("readiness")
+    public ResponseDto<?> readiness(){
+        return ResponseDto.ok("readiness 체크 성공");
+    }
+
+    @GetMapping("/startup")
+    public ResponseDto<?> startup(){
+        return ResponseDto.ok("startup 체크 성공");
+    }
+
+    @GetMapping("liveness")
+    public ResponseDto<?> liveness(){
+        return ResponseDto.ok("liveness 체크 성공");
+    }
 }
